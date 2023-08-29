9202c3d40629d4069ad23252f3dd8dbfe10e8289
Rishi Chakraborty <69898064+Rishi1502@users.noreply.github.com>
2023-08-29 23:07:03 +0530

    day1

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Footer from "./Footer"
+import PopularProducts from "./PopularProducts"
+import Services from "./Services"
+import SpecialOffers from "./SpecialOffers"
+import Subscribe from "./Subscribe"
+import SuperQuality from "./SuperQuality"
+import CustomerReviews from "./CustomerReviews"
+import Hero from "./Hero"
+
+export{
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Subscribe,
+    CustomerReviews,
+    Services,
+    Footer,
+    SpecialOffers
+}
